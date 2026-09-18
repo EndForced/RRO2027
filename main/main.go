4352162bbae4740f6ef4e8a0ae25479e581ce55b
Taras <evafagsclub@gmail.com>
2026-09-19 05:20:53 +1000

    testing shit

--- main/main.go
+++ main/main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
 
-	tele "gopkg.in/telebot.v4"
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки файла .env! Убедитесь, что вы создали его на основе .env.example")
+	}
+
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("Переменная TELEGRAM_BOT_TOKEN не найдена в файле .env")
+	}
+
+	fmt.Print(botToken)
+
 	pref := tele.Settings{
-		Token:  os.Getenv("TOKEN"),
+		Token:  botToken,
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
 	}
 
 	b, err := tele.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
 	b.Handle("/start", func(c tele.Context) error {
-		text := `Привет! Это генератор расстановок для категории будущие инженеры
-Сгенерировать: /gen
-Решить последнюю расстановку: /solve
-
-Расстановки соответствуют <a href="cdn.robofinist.ru/files/339318/filename/RRO-2026-Будущие инженеры.pdf">регламенту категории</a> на 18.09.26`
-
-		// Обязательно передаем tele.ModeHTML, чтобы Telegram распарсил тег <a>
-		return c.Send(text, tele.ModeHTML)
+		return c.Send("Бот успешно запущен в безопасном режиме!")
 	})
 
+	log.Println("Бот успешно стартовал...")
 	b.Start()
 }
